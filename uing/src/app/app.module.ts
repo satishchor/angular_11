700635b50640135298153c29c7a25b121c5f902f
satishchor <chorghesatish8@outlook.com>
2021-03-23 20:57:14 +0530

    added new component

--- uing/src/app/app.module.ts
+++ uing/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HorizontalComponent } from './components/layouts/horizontal/horizontal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorizontalComponent
   ],
   imports: [
     BrowserModule,
