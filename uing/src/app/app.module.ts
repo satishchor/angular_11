e02458bed36b5dd9104696c69cb14f613af7a6e8
satishchor <chorghesatish8@outlook.com>
2021-03-23 20:59:19 +0530

    Revert "added new component"
    This reverts commit 700635b50640135298153c29c7a25b121c5f902f.

--- uing/src/app/app.module.ts
+++ uing/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HorizontalComponent } from './components/layouts/horizontal/horizontal.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HorizontalComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
